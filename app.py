e0f6a55c9642057f843f03bd017d83a2c02eaf72
aabeey <azzabelva@gmail.com>
2026-02-09 07:12:55 +0700

    app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.markdown("""
 <style>
 h2 { text-align: center; white-space: nowrap; }
 div.stButton > button,
-div.stButton > buttonhover,
+div.stButton > button:hover,
 div.stButton > button:active, 
 div.stButton > button:focus {
     background-color: #8e918f !important;
